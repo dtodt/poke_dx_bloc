afd36e3565af06e0d02963ab85d8210e5a0adb65
dtodt <danieltodt@gmail.com>
2022-04-14 18:22:17 -0300

    chore starting point

---
+++ lib/app/app_widget.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Poke DX',
+      theme: ThemeData(
+        brightness: Brightness.light,
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primarySwatch: Colors.red,
+        visualDensity: VisualDensity.adaptivePlatformDensity,
+      ),
+      home: Container(),
+    );
+  }
+}
