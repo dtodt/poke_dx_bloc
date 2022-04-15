a61b63326ba5404938080bad03291451f3849d38
dtodt <danieltodt@gmail.com>
2022-04-15 20:32:59 -0300

    chore: mixin to include a run once method

---
+++ lib/app/core/presentation/mixins/state_ready_aware.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+///
+mixin StateReadyAware<T extends StatefulWidget> on State<T> {
+  void ready();
+
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance?.addPostFrameCallback((_) => ready());
+  }
+}
