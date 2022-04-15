cd247464fa15ed6d3b476cdd1f8f8407f4ebc9bc
dtodt <danieltodt@gmail.com>
2022-04-15 12:16:23 -0300

    chore: pokemon response class

--- test/constants/pokedex.dart
+++ test/constants/pokedex.dart
@@ -1,13 +1,36 @@
 import 'package:pokedx/app/pokedex/domain/entities/pokemon.dart';
 
 ///? Pokemon Entity
+const kCharizardMap = {
+  'name': 'charizard',
+  'url': 'https://pokeapi.co/api/v2/pokemon/6/'
+};
 const kMewMap = {
   'name': 'mew',
   'url': 'https://pokeapi.co/api/v2/pokemon/151/'
 };
 
+const kCharizard = Pokemon(
+  number: '6',
+  name: 'Charizard',
+  url: 'https://pokeapi.co/api/v2/pokemon/6/',
+);
 const kMew = Pokemon(
   number: '151',
   name: 'Mew',
   url: 'https://pokeapi.co/api/v2/pokemon/151/',
 );
+
+///? Pokemon Api response
+const kCharizardResponseMap = {
+  'count': 1,
+  'next': 'https://pokeapi.co/api/v2/pokemon?offset=6&limit=1',
+  'previous': 'https://pokeapi.co/api/v2/pokemon?offset=4&limit=1',
+  'results': [kCharizardMap]
+};
+const kMewResponseMap = {
+  'count': 1,
+  'next': null,
+  'previous': null,
+  'results': [kMewMap]
+};
