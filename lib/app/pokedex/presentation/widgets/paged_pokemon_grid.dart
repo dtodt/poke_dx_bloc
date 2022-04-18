3135464728cd0f8efdea9081a3b164ff7a1e0a5e
dtodt <danieltodt@gmail.com>
2022-04-18 00:39:53 -0300

    chore: pokemon card applied

--- lib/app/pokedex/presentation/widgets/paged_pokemon_grid.dart
+++ lib/app/pokedex/presentation/widgets/paged_pokemon_grid.dart
@@ -1,5 +1,8 @@
+import 'package:cached_network_image/cached_network_image.dart';
+import 'package:cached_network_image_platform_interface/cached_network_image_platform_interface.dart';
 import 'package:flutter/material.dart';
 import 'package:pokedx/app/pokedex/domain/entities/pokemon.dart';
+import 'package:pokedx/app/pokedex/presentation/widgets/pokemon_card.dart';
 
 /// A gridview of pokemon.
 class PagedPokemonGrid extends StatelessWidget {
@@ -14,21 +17,30 @@ class PagedPokemonGrid extends StatelessWidget {
   Widget build(BuildContext context) {
     return SliverGrid(
       delegate: SliverChildBuilderDelegate(
-        (context, index) => Card(
-          child: Center(
-            child: Text(
-              data[index].name,
-              style: Theme.of(context).textTheme.headline6,
+        (context, index) => PokemonCard(
+          key: ValueKey('${data[index].number}-${data[index].name}'),
+          color: Theme.of(context).colorScheme.background,
+          name: data[index].name,
+          number: data[index].number,
+          portrait: CachedNetworkImage(
+            errorWidget: (context, url, error) => const Icon(Icons.error),
+            fit: BoxFit.contain,
+            imageRenderMethodForWeb: ImageRenderMethodForWeb.HtmlImage,
+            imageUrl: data[index].portrait,
+            placeholder: (context, url) => const Center(
+              child: CircularProgressIndicator.adaptive(),
             ),
+            useOldImageOnUrlChange: true,
           ),
+          onTap: () {},
         ),
         childCount: data.length,
       ),
       gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
         childAspectRatio: 1.5,
+        crossAxisCount: 2,
         crossAxisSpacing: 10,
         mainAxisSpacing: 10,
-        crossAxisCount: 2,
       ),
     );
   }
