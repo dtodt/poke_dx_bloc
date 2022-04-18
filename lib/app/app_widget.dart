0e860afba59aa45e8f75b4a69dcf6e45128fe6e8
dtodt <danieltodt@gmail.com>
2022-04-18 00:03:14 -0300

    chore: dark mode only and page transitions web

--- lib/app/app_widget.dart
+++ lib/app/app_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:pokedx/app/core/core_module.dart';
+import 'package:pokedx/app/core/presentation/helpers/page_transitions_helper.dart';
 import 'package:pokedx/app/pokedex/pokedex_module.dart';
 import 'package:pokedx/app/pokedex/presentation/pages/pokemon_list_page.dart';
 import 'package:provider/provider.dart';
@@ -14,15 +15,12 @@ class AppWidget extends StatelessWidget {
         debugShowCheckedModeBanner: false,
         title: 'Poke DX',
         theme: ThemeData(
-          brightness: Brightness.light,
-          primarySwatch: Colors.red,
-          visualDensity: VisualDensity.adaptivePlatformDensity,
-        ),
-        darkTheme: ThemeData(
           brightness: Brightness.dark,
+          pageTransitionsTheme: PageTransitionsHelper(),
           primarySwatch: Colors.red,
           visualDensity: VisualDensity.adaptivePlatformDensity,
         ),
+        themeMode: ThemeMode.dark,
         home: const PokemonListPage(),
       ),
       providers: [
