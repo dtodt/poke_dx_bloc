cec1807601f78a76a435629b142f385f23dd4d25
dtodt <danieltodt@gmail.com>
2022-04-18 00:04:04 -0300

    chore: image loader

---
+++ lib/app/configs/images.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const String _imagePath = 'images';
+
+class _Image extends AssetImage {
+  const _Image(String fileName) : super('$_imagePath/$fileName');
+}
+
+class AppImages {
+  static const pokeball = _Image('pokeball.png');
+
+  static Future precacheAssets(BuildContext context) async {
+    await precacheImage(pokeball, context);
+  }
+}
