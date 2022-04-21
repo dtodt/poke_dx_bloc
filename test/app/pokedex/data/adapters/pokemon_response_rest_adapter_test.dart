ff00390902551d8208ec2133316b63c8de167e0b
dtodt <danieltodt@gmail.com>
2022-04-20 22:54:39 -0300

    chore: test assurance

--- test/app/pokedex/data/adapters/pokemon_response_rest_adapter_test.dart
+++ test/app/pokedex/data/adapters/pokemon_response_rest_adapter_test.dart
@@ -10,11 +10,17 @@ void main() {
         PokemonResponseRestAdapter.fromMap(kCharizardRestResponseMap);
     expect(result, isA<PokemonResponse>());
     expect(result.results, isNotEmpty);
+    expect(result.next, isNotNull);
+    expect(result.next!.offset, equals(6));
+    expect(result.previous, isNotNull);
+    expect(result.previous!.offset, equals(4));
   });
 
   test('should convert a map with null into pokemon response', () async {
     final result = PokemonResponseRestAdapter.fromMap(kMewRestResponseMap);
     expect(result, isA<PokemonResponse>());
     expect(result.results, isNotEmpty);
+    expect(result.next, isNull);
+    expect(result.previous, isNull);
   });
 }
